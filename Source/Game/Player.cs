bdc1b9809475196dfbf425789ffe3cf2b4dade74
ernestdodz <ernestdodz@gmail.com>
2023-10-31 17:07:20 +0800

    first commit

---
+++ Source/Game/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using FlaxEngine;
+
+namespace Game
+{
+    public class Player : Script
+    {
+
+        public RigidBody rigidbody;
+
+        public override void OnStart()
+        {
+          
+        }
+
+        public override void OnUpdate()
+        {
+
+
+        }
+    }
+}
